d43f621adc691046b325964b8ba3fe05fdb99371
Tomirio <tomirio619@gmail.com>
2017-03-24 17:25:39 +0100

    Fixed MixColumn implementation, encryption is now working

--- Midori.py
+++ Midori.py
@@ -1,5 +1,7 @@
 from math import floor
+
 import numpy as np
+
 from constants import roundConstants
 from sboxes import Sb1
 
@@ -103,12 +105,112 @@ def MixColumn(state):
     print("MixColumn, 'before' state:\n{}\n".format(state))
     print("Matrix M is defined as follows\n{}\n".format(m))
     for col in range(4):
-        print("We are going to multiply M with column {} \n{}\n".format(col, state[:, col]))
-        state[:, col] = m.dot(state[:, col]) % 256
-        print("Column {}, 'after' the multiplication with M:\n{}\n".format(col, state[:, col]))
-        print("MixColumn 'new' state:\n{}\n".format(state))
+        stateCol = np.squeeze(np.asarray(state[:, col]))
+        state[:, col] = UpdateColumn(m, stateCol)
     print("MixColumn 'final' state:\n{}\n".format(state))
     return state
+
+
+def UpdateColumn(m, col):
+    results = np.zeros(4, dtype=np.uint8)
+    for row in range(4):
+        matrixRow = np.squeeze(np.asarray(m[row, :]))
+        print("The matrix row:", matrixRow)
+        print("The affected column:", col)
+        for (a, b) in zip(matrixRow, col):
+            results[row] ^= PolyMult(a, b)
+    newColumn = np.transpose(np.asmatrix(results))
+    return newColumn
+
+
+def PolyMult(p1, p2):
+    """
+    Multiply two numbers in the GF(2^8) finite field defined
+    by the polynomial x^8 + x^4 + x^3 + x + 1 = 0
+    See http://stackoverflow.com/questions/13202758/multiplying-two-polynomials
+    :param p1: Polynomial a
+    :param p2: Polynomial b
+    :return: a * b mod (x^8 + x^4 + x^3 + x + 1)
+    """
+    PrintPoly(p1)
+    print(" * ", end="")
+    PrintPoly(p2)
+    print(" = ", end="")
+    binP2 = bin(p2)[2:].zfill(8)
+    mult = 0
+    # Base case
+    if p1 == 0 or p2 == 0:
+        PrintPoly(mult)
+        print("\n")
+        return 0
+    # Perform the multiplication
+    for i in range(8):
+        bit = binP2[i]
+        if bit == "1":
+            mult ^= (p1 << (7 - i))
+    PrintPoly(mult)
+    print("\n")
+    # Peform the reduction
+    reducPoly = int("100011011", 2)
+    while True:
+        if GetMSBIndex(mult) < GetMSBIndex(reducPoly):
+            break
+        elif GetMSBIndex(mult) == GetMSBIndex(reducPoly):
+            mult ^= reducPoly
+        else:
+            degreeDiff = GetMSBIndex(mult) - GetMSBIndex(reducPoly)
+            mult ^= (reducPoly << degreeDiff)
+    print("After reducing, the result becomes:")
+    PrintPoly(mult)
+    print("\n")
+    return mult
+
+
+def GetMSBIndex(n):
+    """
+     Given a bit sequence, indicate the index of the most significant
+    set bit, where the index of the least significant bit is zero.
+    :param n: The number to determine the MSB bit of.
+    :return: index of the MSB of n.
+    """
+    ndx = 0
+    while 1 < n:
+        n = (n >> 1)
+        ndx += 1
+    return ndx
+
+
+def GetLSBIndex(n):
+    """Returns the index, counting from 0, of the
+    least significant set bit in `n`.
+    """
+    return GetMSBIndex(n & -n)
+
+
+def PrintPoly(n):
+    """
+    Prints a number in the corresponding polynomial representation.
+    In this representation, the MSB bit represents the higest term in the polynomial,
+    and the LSB lowest term.
+    :param n: number
+    """
+    if n == 0:
+        print("(0)", end="")
+        return
+    print("(", end="")
+    msbIndex = GetMSBIndex(n)
+    lsbIndex = GetLSBIndex(n)
+    for i in range(msbIndex + 1, 0, -1):
+        if (n >> i) & 1 > 0:
+            if i >= 10:
+                print("x^{{{}}}".format(i), end="")
+            else:
+                print("x^{}".format(i), end="")
+            if i != lsbIndex:
+                print(" + ", end="")
+    if n & 1 > 0:
+        print("1", end="")
+    print(")", end="")
 
 
 def SplitByN(seq, n):
@@ -186,14 +288,13 @@ def InitializeState(binaryString):
     :return: The state that corresponds to the given binary string.
     """
     state = np.zeros(shape=(4, 4), dtype=np.uint8)
-    print(state)
     # Again, s0 contains the 8 most significant bits of the plaintext, and s15 the least significant ones
     plaintextBytes = SplitByN(binaryString, 8)
     for col in range(4):
         for row in range(4):
             binary = plaintextBytes[col * 4 + row]
             state[row, col] = int(binary, 2)
-    print("The initial state is as follows:\n{}\n".format(state))
+    print("The initial state:\n{}\n".format(state))
     return state
 
 
@@ -206,12 +307,10 @@ def MidoriEncrypt(plaintext, key):
     """
     plaintext = plaintext[2:].zfill(128)  # Remove the binary prefix in the binary strings, and pad with zeros
     key = key[2:].zfill(128)
-    print("plaintext:\t{}\nkey:\t\t{}\n".format(plaintext, key))
     r = 20  # Number of rounds
     state = InitializeState(plaintext)
     state = KeyAdd(state, key)
-    print("State after KeyAdd:\n{}\n".format(StateToBinary(state)))
-    print("Hex state:\n{0:02x}\n".format(int(StateToBinary(state), 2)))
+    print("Hex:\n{0:02x}\n".format(int(StateToBinary(state), 2)))
     RKs = RoundKeyGen(key)
     # This is where the magic happens
     for i in range(r - 1):
@@ -235,12 +334,14 @@ def main():
     # Convert input, given as hex, to binary (padded with zeros)
     plaintext = bin(0x51084CE6E73A5CA2EC87D7BABC297543)
     key = bin(0x687DED3B3C85B3F35B1009863E2A8CBF)
-    ciphertext = "0x1E0AC4FDDFF71B4C1801B73EE4AFC83D"
+    ciphertext = "0x1E0AC4FDDFF71B4C1801B73EE4AFC83D".lower()
     c = MidoriEncrypt(plaintext, key)
     if c != ciphertext:
         print("The ciphertexts were not the same.")
         print("Expected ciphertext:\t{}".format(ciphertext))
         print("Calculated ciphertext:\t{}".format(c))
+    else:
+        print("The ciphertexts were the same!")
 
 
 if __name__ == "__main__":
